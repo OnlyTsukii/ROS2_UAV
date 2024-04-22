62d74637fc34863f9f0e59eb5fb734f59c6b4b1f
Changlong Cao <68173779+OnlyTsukii@users.noreply.github.com>
2024-04-22 17:26:21 +0800

    Delete build directory

--- build/uav_urdf_demo/CMakeFiles/uav_urdf_demo_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/uav_urdf_demo_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/uav_urdf_demo_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
